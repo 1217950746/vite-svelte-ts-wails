544a8e4a15afc0a68ab2d9c4fb4fd6b90d1d3859
ONEO <oneo.me@outlook.com>
2022-06-09 16:21:53 +0800

    Initial commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Beach_Regular from "svelte-fluentui-icons/icons/Beach_Regular.svelte";
+    import { GetName } from "../wailsjs/go/main/App.js";
+
+    document.body.className = "antialiased bg-white text-slate-600 text-base";
+
+    let name = "";
+    let title = "";
+
+    GetName().then(function (newName) {
+        name = newName;
+        title = name + " Template";
+    });
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<div class="flex items-center h-screen place-content-center">
+    <div class="flex flex-col items-center space-y-6">
+        <Beach_Regular size={180} class="text-blue-600" />
+        <h1 class="text-4xl font-bold">Hello {name}</h1>
+        <p>Vite + Svelte + TS + Tailwindcss + {name}</p>
+    </div>
+</div>
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+)
+
+//go:embed frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:     "testApp",
+		Width:     1024,
+		Height:    768,
+		Assets:    assets,
+		OnStartup: app.startup,
+		Bind: []interface{}{
+			app,
+		},
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: true,
+				HideTitle:                  false,
+				HideTitleBar:               false,
+				FullSizeContent:            false,
+				UseToolbar:                 false,
+				HideToolbarSeparator:       true,
+			},
+			Appearance:           mac.DefaultAppearance,
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  false,
+			About: &mac.AboutInfo{
+				Title:   "My Application",
+				Message: "© 2021 Me",
+			},
+		},
+	})
+
+	if err != nil {
+		println("Error:", err)
+	}
+}
